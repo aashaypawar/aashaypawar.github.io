782ba2f2ce233fe874cf558a6a95c1a48d88f5dc
Aashay Pawar <aapawar26@gmail.com>
2020-10-08 00:06:17 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,17 @@
         }
       }
     });
+
+    //Typed
+    var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
   
     // Scroll to top button appear
     $(document).scroll(function() {
